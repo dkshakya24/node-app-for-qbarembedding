1ee105797912c17026428ad3ce313bed22a10e32
shakyadeep <shakyadeep@shakyadeeps-MacBook-Air.local>
2024-05-02 01:29:34 +0530

    changes for quicksight

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const {
+  QuickSightClient,
+  GenerateEmbedUrlForRegisteredUserCommand,
+} = require("@aws-sdk/client-quicksight");
+
 const app = express();
 const port = process.env.PORT || 3000; // Use environment variable or default port
 
@@ -8,6 +13,36 @@ app.get("/api/data", (req, res) => {
   const data = { message: "Hello from the API!" };
 
   res.json(data); // Send data as JSON
+});
+app.get("/generate-embed-url", async (req, res) => {
+  try {
+    // Create QuickSight client
+    const client = new QuickSightClient({ region: "us-west-2" });
+
+    // Construct command
+    const command = new GenerateEmbedUrlForRegisteredUserCommand({
+      AwsAccountId: "603640115098",
+      UserArn:
+        "arn:aws:quicksight:us-west-2:603640115098:user/default/deepak.kumar",
+      ExperienceConfiguration: {
+        QNRESearchBar: {
+          InitialTopicId: "kjhkjhjh",
+        },
+      },
+      // AllowedDomains: ['http://localhost:5173/'],
+      SessionLifetimeInMinutes: 600,
+    });
+
+    // Send command and await response
+    const response = await client.send(command);
+
+    // Send embed URL in the response
+    res.json({ embedUrl: response.EmbedUrl });
+  } catch (error) {
+    // Handle errors
+    console.error("Error generating embed URL:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(port, () => {
